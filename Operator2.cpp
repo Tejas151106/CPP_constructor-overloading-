fd4343b77102b3f0c2d8d518c2162098c49b89ac
Tejas151106 <gandlurisaitejasnvn@gmail.com>
2025-09-17 11:23:33 +0530

    Create Operator2.cpp

---
+++ Operator2.cpp
@@ -0,0 +1,29 @@
+//Gandluri Sai Tejas
+//24070123045
+#include <iostream>
+using namespace std;
+#include<string>
+
+class Name{
+public:
+string name1; 
+string name2;
+
+Name(string n1, string n2){
+    name1=n1;   
+    name2=n2;   
+}
+
+void add(){
+   cout<<name1+name2;  
+}      
+};
+int main() 
+{
+    Name N1("Ishika","Singhal");
+    N1.add();
+
+}
+
+//Output
+//IshikaSinghal
